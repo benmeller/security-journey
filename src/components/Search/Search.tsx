6f3539bd5f8091021e00343e88d04ce23831dd1f
benmeller <ben.meller3@gmail.com>
2024-12-02 22:58:26 +1100

    Search - Click to close

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -70,38 +70,53 @@ export default function Search({ searchList }: { searchList?: any}) {
 		}
 	};
 
-	// TODO: handle outside click
+	const handleDialogClick = (event: React.MouseEvent<HTMLDialogElement>) => {
+		if (event.type === "click" && event.target === searchRef.current) {
+			handleCloseModal();
+		}
+	}
+
 	// TODO: add close button to modal
 	// TODO: add styling
 	// TODO: Darken background
 
     return (
 		<>
-			<div>
-				<button onClick={() => setIsOpen(true)}>Click me for search</button>
-			</div>
-			<dialog ref={searchRef} onKeyDown={handleKeyDown}>
-				<label>Search</label>
-				<input 
-					type="text" 
-					value={query} 
-					onChange={handleOnSearch} 
-					placeholder="Search posts" 
-				/>
-				{query.length > 1 && (
-					<p>
-						Found {results.length} {results.length === 1 ? 'result' : 'results'} for '{query}'
-					</p>
-				)}
-				<ul>
-					{results &&
-						results.map((item) => (
-							<li>
-								<a href={`/${item.slug}`}>{item.title}</a>
-								{item.description}
-							</li>
-						))}
-				</ul>
+			<button onClick={() => setIsOpen(true)} style={{ border: 0, background: "none" }}>
+				<svg 
+					xmlns="http://www.w3.org/2000/svg" 
+					width="32"
+					style={{ fill: "rgb(var(--black))" }}
+					viewBox="0 0 256 256"
+				>
+					<path d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z"></path>
+				</svg>
+			</button>
+			<dialog ref={searchRef} onKeyDown={handleKeyDown} onClick={handleDialogClick}
+			>
+				<div>
+					<label>Search</label>
+					<input 
+						type="text" 
+						value={query} 
+						onChange={handleOnSearch} 
+						placeholder="Search posts" 
+					/>
+					{query.length > 1 && (
+						<p>
+							Found {results.length} {results.length === 1 ? 'result' : 'results'} for '{query}'
+						</p>
+					)}
+					<ul>
+						{results &&
+							results.map((item) => (
+								<li>
+									<a href={`/${item.slug}`}>{item.title}</a>
+									{item.description}
+								</li>
+							))}
+					</ul>
+				</div>
 			</dialog>
 		</>
 	);
